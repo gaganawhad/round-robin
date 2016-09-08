8a4836523e4588e62eacbed1e6038d07be930584
Gagan Awhad <gagan.a.awhad@gmail.com>
2016-09-07 20:44:42 -0500

    Move tests to spec folder

--- lib/track.rb
+++ lib/track.rb
@@ -54,38 +54,4 @@ module RoundRobin
       end
     end
   end
-
-  describe Track do
-    def redis
-      @redis ||= Redis.new(url: 'redis://localhost:6379')
-    end
-
-    before do
-      redis.flushall
-    end
-
-    describe '#cycle' do
-      it 'cycles through the list' do
-        track = Track.new('customer-support', ['Andy', 'Sandy', 'George'])
-        track.cycle
-        expect(track.list).to eq ['Sandy', 'George', 'Andy']
-      end
-    end
-
-    describe '#next' do
-      it 'returns the element at the head of the list and cycles through it' do
-        track = Track.new('customer-support', ['Andy', 'Sandy', 'George'])
-        expect(track.next).to eq 'Andy'
-        expect(track.list).to eq ['Sandy', 'George', 'Andy']
-      end
-    end
-
-    describe '#add' do
-      it 'adds an element to the end of the list' do
-        track = Track.new('customer-support', ['Andy', 'Sandy', 'George'])
-        track.add 'Sam'
-        expect(track.list).to eq ['Andy', 'Sandy', 'George', 'Sam' ]
-      end
-    end
-  end
 end
